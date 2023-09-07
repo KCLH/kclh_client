7e265ff011d0894d55a2b96ee6c577710aad5d61
kim228sy <kim228sy@gmail.com>
2023-09-07 17:22:55 +0900

    헤더 수정 중 문제 발생, 수정 중

---
+++ client/components/hooks/DropdownMenuButton.tsx
@@ -0,0 +1,70 @@
+import { Button, IconButton, Avatar, Menu, MenuItem } from "@mui/material";
+import { AccountCircleRounded } from "@mui/icons-material/";
+import { MouseEvent } from "react";
+
+interface MenuItem {
+  name: string;
+  url: string;
+}
+
+interface DropdownMenuButtonProps {
+  buttonContent: string | JSX.Element;
+  menuItems: MenuItem[];
+  currentMenu: string;
+  anchorEl: HTMLElement | null;
+  handleClick(event: MouseEvent<HTMLElement>): void;
+  handleClose(url?: string): void;
+}
+
+export default function DropdownMenuButton({
+  buttonContent,
+  menuItems,
+  currentMenu,
+  anchorEl,
+  handleClick,
+  handleClose,
+}: DropdownMenuButtonProps) {
+  const open = Boolean(anchorEl);
+  const id = typeof buttonContent === "string" ? buttonContent : "account";
+
+  return (
+    <>
+      {typeof buttonContent === "string" ? (
+        <Button
+          sx={{
+            color: "#262626",
+            fontSize: "20px",
+            fontWeight: 700,
+          }}
+          aria-controls={open ? `${id}-menu` : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+          onClick={handleClick}
+        >
+          {buttonContent}
+        </Button>
+      ) : (
+        <IconButton onClick={(event) => handleClick(event)} size="small">
+          <Avatar sx={{ width: 32, height: 32 }}>
+            <AccountCircleRounded />
+          </Avatar>
+          //{" "}
+        </IconButton>
+      )}
+      <div>
+        <Menu
+          id={`${id}-menu`}
+          anchorEl={anchorEl}
+          open={open && currentMenu === id}
+          onClose={() => handleClose()}
+        >
+          {menuItems.map((item) => (
+            <MenuItem key={item.name} onClick={() => handleClose(item.url)}>
+              {item.name}
+            </MenuItem>
+          ))}
+        </Menu>
+      </div>
+    </>
+  );
+}
--- client/components/signUp/signUp.presenter.tsx
+++ client/components/signUp/signUp.presenter.tsx
@@ -1,17 +1,7 @@
 "use client";
 
-import {
-  TextField,
-  Button,
-  Box,
-  // Select,
-  // MenuItem,
-  // InputLabel,
-  // FormControl,
-  // SelectChangeEvent,
-} from "@mui/material";
+import { TextField, Button, Box } from "@mui/material";
 import styled from "@emotion/styled";
-// import { useState } from "react";
 import {
   DEPARTMENTS,
   RANKS,
@@ -39,27 +29,6 @@ const BtnWrapper = styled.div`
 `;
 
 export default function SignUpUI(props: SignUpUIProps) {
-  // const [department, setDepartment] = useState("");
-  // const [rank, setRank] = useState("");
-  // const [factory, setFactory] = useState("");
-  // const [role, setRole] = useState("");
-
-  // const handleDepartmentChange = (event: SelectChangeEvent) => {
-  //   setDepartment(event.target.value);
-  // };
-
-  // const handleRankChange = (event: SelectChangeEvent) => {
-  //   setRank(event.target.value);
-  // };
-
-  // const handleFactoryChange = (event: SelectChangeEvent) => {
-  //   setFactory(event.target.value);
-  // };
-
-  // const handleRoleChange = (event: SelectChangeEvent) => {
-  //   setRole(event.target.value);
-  // };
-
   return (
     <Wrapper>
       <h1 style={{ padding: "5px 0", color: "#262626" }}>사원 등록</h1>
@@ -98,88 +67,20 @@ export default function SignUpUI(props: SignUpUIProps) {
           variant="filled"
         />
 
-        {/* <FormControl fullWidth variant="filled" sx={{ m: 1.5, minWidth: 120 }}>
-          <InputLabel id="department-label">부서</InputLabel>
-          <Select
-            {...props.register("department")}
-            labelId="demo-simple-select-filled-label"
-            id="demo-simple-select-filled"
-            value={department}
-            onChange={handleDepartmentChange}
-          >
-            <MenuItem value="">
-              <em>선택</em>
-            </MenuItem>
-            {DEPARTMENTS.map((department) => (
-              <MenuItem value={department}>{department}</MenuItem>
-            ))}
-          </Select>
-        </FormControl> */}
         <CustomSelect
           label="부서"
           options={DEPARTMENTS}
           register={props.register}
         />
 
-        {/* <FormControl fullWidth variant="filled" sx={{ m: 1.5, minWidth: 120 }}>
-          <InputLabel id="rank-label">직급</InputLabel>
-          <Select
-            {...props.register("rank")}
-            labelId="demo-simple-select-filled-label"
-            id="demo-simple-select-filled"
-            value={rank}
-            onChange={handleRankChange}
-          >
-            <MenuItem value="">
-              <em>선택</em>
-            </MenuItem>
-            {RANKS.map((rank) => (
-              <MenuItem value={rank}>{rank}</MenuItem>
-            ))}
-          </Select>
-        </FormControl> */}
         <CustomSelect label="직급" options={RANKS} register={props.register} />
 
-        {/* <FormControl fullWidth variant="filled" sx={{ m: 1.5, minWidth: 120 }}>
-          <InputLabel id="factory-label">공장</InputLabel>
-          <Select
-            {...props.register("factory")}
-            labelId="demo-simple-select-filled-label"
-            id="demo-simple-select-filled"
-            value={factory}
-            onChange={handleRankChange}
-          >
-            <MenuItem value="">
-              <em>선택</em>
-            </MenuItem>
-            {FACTORY.map((fac) => (
-              <MenuItem value={fac}>{fac}</MenuItem>
-            ))}
-          </Select>
-        </FormControl> */}
         <CustomSelect
           label="공장"
           options={FACTORY}
           register={props.register}
         />
 
-        {/* <FormControl fullWidth variant="filled" sx={{ m: 1.5, minWidth: 120 }}>
-          <InputLabel id="role-label">권한</InputLabel>
-          <Select
-            {...props.register("adminOk")}
-            labelId="demo-simple-select-filled-label"
-            id="demo-simple-select-filled"
-            value={role}
-            onChange={handleRankChange}
-          >
-            <MenuItem value="">
-              <em>선택</em>
-            </MenuItem>
-            {ROLES.map((role) => (
-              <MenuItem value={role}>{role}</MenuItem>
-            ))}
-          </Select>
-        </FormControl> */}
         <CustomSelect label="권한" options={ROLES} register={props.register} />
 
         <BtnWrapper>
