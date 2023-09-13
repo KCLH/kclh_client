c461ca008778accc736e3e4bf09fcdc31c61a102
BurnBean <cjh2294@gmail.com>
2023-09-13 18:50:36 +0900

    webGL 배치중

--- client/components/factory/factory.presenter.tsx
+++ client/components/factory/factory.presenter.tsx
@@ -1,35 +1,30 @@
 "use client";
 import { Canvas } from "@react-three/fiber";
-import * as THREE from "three";
+
 import { Body } from "@/components/factory/Edukit/Body";
+import { Belt } from "@/components/factory/Edukit/Belt";
+import { TrafficLight_Green } from "@/components/factory/TrafficLight/Green";
 
 export default function FactoryUI(props: any) {
   // console.log(props);
 
   return (
     <>
-      <>{props.idFromPath}</>
-      <div className="home" style={{ backgroundColor: "gray" }}>
-        <Canvas camera={{ position: [1000, 0, 0] }}>
-          <directionalLight position={[10, 10, 10]} />
+      <div style={{ backgroundColor: "#fff", width: "100%", height: "100vh" }}>
+        <Canvas camera={{ position: [1, 1, -1] }}>
+          <directionalLight position={[2, 1, 1]} intensity={5} />
           <Body />
           {/* <Belt />
-          <ColorSensor_Body />
-          <ColorSensor_G />
+          <TrafficLight_Green /> */}
+          {/* <ColorSensor_G />
           <ColorSensor_R />
-          <M1_Body />
           <M1_Pusher />
-          <M2_Body />
           <M2_Pusher />
-          <M3_Body />
           <M3_Gripper />
           <M3_Yaxis />
           <M3_Ybar />
-          <TrafficLight_Body />
-          <TrafficLight_Green />
           <TrafficLight_Red />
-          <TrafficLight_Yellow />
-          <VisionSensor /> */}
+          <TrafficLight_Yellow /> */}
         </Canvas>
       </div>
     </>
