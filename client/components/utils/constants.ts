8d5e6ff7cf75dd2a036d7b41070d9b77ebc7c3c3
kim228sy <kim228sy@gmail.com>
2023-09-14 11:11:13 +0900

    로그인/회원가입 기능수정

--- client/components/utils/constants.ts
+++ client/components/utils/constants.ts
@@ -15,7 +15,8 @@ export const RANKS = ["사원", "주임", "대리", "과장", "차장", "부장"
 
 export const FACTORY = ["전체", "양주 1공장", "파주 2공장"];
 
-export const ROLES = ["사원", "관리자"];
+// export const ROLES = ["사원", "관리자"];
+export const ROLES = ["Y", "N"];
 
 export const PAGES = [
   {
@@ -35,8 +36,10 @@ export const PAGES = [
 ];
 
 export const USERS = [
-  { name: "내 계정", url: "/user/myaccount", roles: ["사원"] },
-  { name: "사원 관리", url: "/admin/accounts", roles: ["관리자"] },
+  // { name: "내 계정", url: "/user/myaccount", roles: ["사원"] },
+  // { name: "사원 관리", url: "/admin/accounts", roles: ["관리자"] },
+  { name: "내 계정", url: "/user/myaccount", roles: ["N"] },
+  { name: "사원 관리", url: "/admin/accounts", roles: ["Y"] },
 ];
 
 export const CAROUSEL_IMG = [
